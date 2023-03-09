345928bf01bb885b5003834becb114c80a787223
PC_Maintenance_2203 <PC_Maintenance_2203@HAE-L1180510001.hae.global>
2023-03-09 13:53:45 +0900

    a

---
+++ nettySample/src/main/java/com/socket/nettySample/nettySample/NettySampleApplication.java
@@ -0,0 +1,13 @@
+package com.socket.nettySample.nettySample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettySampleApplication.class, args);
+	}
+
+}
---
+++ nettySample/src/test/java/com/socket/nettySample/nettySample/NettySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socket.nettySample.nettySample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettySampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
